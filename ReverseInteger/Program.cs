4b3cc283e875bb06369ac2f0e283ef52b0d51e2b
Charles Chang <charles.chang@live.com>
2025-12-06 20:00:39 +0800

    submission is accepted.

--- ReverseInteger/Program.cs
+++ ReverseInteger/Program.cs
@@ -4,32 +4,50 @@ public class Solution
 {
     public int Reverse(int x)
     {
-        int lastResult = 0;
-        int currentResult = 0;
+        int result = 0;
         int beginDigitIndex = 0;
 
         string s = x.ToString();
 
         if (s[0] != '-')
         {
-            currentResult = s[0] - '0';
-            lastResult = currentResult;
+            result = s[0] - '0';
             beginDigitIndex++;
         }
 
         for (int i = 1; i < s.Length; i++)
-        {
-            currentResult += (s[i] - '0') * (int)Math.Pow(10, beginDigitIndex++);
-            if (currentResult < lastResult)
-                return 0;
+            result += (s[i] - '0') * (int)Math.Pow(10, beginDigitIndex++);
 
-            lastResult = currentResult;
+        string reverseResult = new string(result.ToString().Reverse().ToArray());
+
+        // cut '0' in the last digits in s.
+        int endIndex = -1;
+        if (s.Length > 1) 
+        { 
+            for (int i= s.Length-1; i >= 0;  i--) 
+            { 
+                if (s[i] != '0')
+                {
+                    endIndex = i;
+                    break;
+                }
+            }
+            if (endIndex != -1)
+                s = s.Substring(0, endIndex + 1);
         }
 
         if (s[0] == '-')
-            return currentResult * (-1);
+        {
+            reverseResult = "-" + reverseResult;
+            if (s != reverseResult)
+                return 0;
 
-        return currentResult;
+            return result * (-1);
+        }
+        if (s != reverseResult)
+            return 0;
+
+        return result;
     }
 
     public bool TestCase(int x, int expected)
@@ -48,10 +66,16 @@ class Program
         bool result;
         Solution test = new Solution();
 
+        result = test.TestCase(10, 1);
+
+        result = test.TestCase(-120, -21);
+
+        result = test.TestCase(120, 21);
+
         result = test.TestCase(1534236469, 0);
 
-
         result = test.TestCase(-123, -321);
+
         result = test.TestCase(0, 0);
 
     }
