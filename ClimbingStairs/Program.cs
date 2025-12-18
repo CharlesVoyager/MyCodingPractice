ba3cfee69855e0d51af70d91e69d817411e4301c
Charles Chang <charles.chang@live.com>
2025-12-18 17:09:50 +0800

    Submisison is accepted.

--- ClimbingStairs/Program.cs
+++ ClimbingStairs/Program.cs
@@ -6,8 +6,37 @@ n: 2, Expected: 2
 n: 3, Expected: 3
 n: 4, Expected: 5
 n: 44, Expected: 1,134,903,170
-*/
+n: 45, Expected: 1,836,311,903
+ */
 
+// Submission is accepeted: Runtime 0ms, Beats 100%.
+public class Solution
+{
+    public int ClimbStairs(int n)
+    {
+        if (n == 1) return 1;
+
+        int currentSteps = 2;
+        int currentDistinctWays = 2;
+        int peviousDistinctWaysEndsWithOneStep = 0;
+        int currentDistinctWaysEndsWithOneStep = 1;
+
+        currentSteps++;
+        while (currentSteps <= n)
+        {
+            int peviousDistinctWaysEndsWithOneStepSave = currentDistinctWaysEndsWithOneStep;
+            currentDistinctWaysEndsWithOneStep += peviousDistinctWaysEndsWithOneStep;
+            currentDistinctWays += currentDistinctWaysEndsWithOneStep;
+            peviousDistinctWaysEndsWithOneStep = peviousDistinctWaysEndsWithOneStepSave;
+            currentSteps++;
+
+        }
+
+        return currentDistinctWays;
+    }
+}
+
+#if false
 // Test case n: 44. The output is correct but time limit exceeded.
 public class Solution
 {
@@ -61,13 +90,24 @@ public class Solution
     }
 }
 
+#endif
+
+
 class Program
 {
     public static void Main()
     {
         Solution test = new Solution();
-        int distinctWays = test.ClimbStairs(44);
+        int distinctWays;
 
-        Console.WriteLine("Steps 44: " + distinctWays.ToString());
+
+        distinctWays = test.ClimbStairs(5);    // Expected: 8
+        Console.WriteLine("Steps 5: " + distinctWays.ToString());
+
+        distinctWays = test.ClimbStairs(4);    // Expected: 5
+        Console.WriteLine("Steps 4: " + distinctWays.ToString());
+
+        //distinctWays = test.ClimbStairs(44);    // Expected: 1,134,903,170
+        //Console.WriteLine("Steps 44: " + distinctWays.ToString());
     }
 }
