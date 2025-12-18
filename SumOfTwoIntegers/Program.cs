0a3718bc64ce7de744220a353b2a78e7235c1dea
Charles Chang <charles.chang@live.com>
2025-12-18 23:25:03 +0800

    Added new problem.

---
+++ SumOfTwoIntegers/Program.cs
@@ -0,0 +1,29 @@
+﻿// 371. Sum of two integers.
+
+public class Solution
+{
+    public int GetSum(int a, int b)
+    {
+        int xorSum = a ^ b;
+        int carry = (a & b) << 1;
+        while (carry != 0)
+        {
+            int tempSum = xorSum ^ carry;
+            carry = (xorSum & carry) << 1;
+            xorSum = tempSum;
+        }
+        return xorSum;
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Solution test = new Solution();
+        int result;
+        result = test.GetSum(1, 3); // Output: 14, Expected: 16
+        //result = test.GetSum(9, 7); // Output: 14, Expected: 16
+        Console.WriteLine(result);
+    }
+}
