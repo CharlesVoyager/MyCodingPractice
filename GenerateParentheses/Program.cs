15a35928523886e88930adf8182bd66d8eb52435
Charles Chang <charles.chang@live.com>
2025-12-19 16:03:35 +0800

    Added new problem and submission is accepted.

---
+++ GenerateParentheses/Program.cs
@@ -0,0 +1,73 @@
+﻿// 22. Generate Parentheses
+
+public class Solution
+{
+    int targetN = 0;
+    List<string> results = new List<string>();
+
+    void AddLeftParenthes(string currentStr)
+    {
+        int leftCount = 0;
+        foreach (char c in currentStr)
+            if (c == '(')
+                leftCount++;
+
+        if (leftCount < targetN)
+        {
+            AddLeftParenthes(currentStr + "(");
+
+            int rightCount = 0;
+            foreach (char c in currentStr)
+                if (c == ')')
+                    rightCount++;
+
+            if (rightCount < targetN)
+                AddRightParenthes(currentStr + ")");
+        }
+        else
+            AddRightParenthes(currentStr + ")");
+
+    }
+
+    void AddRightParenthes(string currentStr)
+    {
+        int leftCount = 0;
+        foreach (char c in currentStr)
+            if (c == '(')
+                leftCount++;
+
+        int rightCount = 0;
+        foreach (char c in currentStr)
+            if (c == ')')
+                rightCount++;
+
+        if (leftCount < targetN)
+            AddLeftParenthes(currentStr + "(");
+
+        if (rightCount < leftCount)
+            AddRightParenthes(currentStr + ")");
+      
+        if (rightCount == targetN)
+            results.Add(currentStr);
+    }
+
+    public IList<string> GenerateParenthesis(int n)
+    {
+        targetN = n;
+        AddLeftParenthes("(");
+        return results;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Solution test = new Solution();
+
+        IList<string> results = test.GenerateParenthesis(8);  
+
+        foreach (string s in results)
+            Console.WriteLine(s);
+    }
+}
---
+++ MyTest/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        float a = 1.239f;
+
+        Console.WriteLine(a);
+
+
+        float b = 1234567890123f;
+
+        float c = 12345678901999f;
+        Console.WriteLine(b);
+
+        if (b == c)
+            Console.WriteLine("b equals c");
+        else
+        {
+            Console.WriteLine("b NOT equals c");
+        }
+    }
+}
