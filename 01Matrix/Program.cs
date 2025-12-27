603f116243de209e4acdedfe3943240ba1b8d0e8
Charles Chang <charles.chang@live.com>
2025-12-27 14:47:09 +0800

    Added alternative solution for 01 Matrix problem.

--- 01Matrix/Program.cs
+++ 01Matrix/Program.cs
@@ -4,6 +4,8 @@ public class Solution
 {
     List<(int, int)> verticesToProcess = new List<(int, int)>();
     int currentDistance = 1;
+
+#if true
     public int[][] UpdateMatrix(int[][] mat)
     {
         // Initialize verticesToProcess
@@ -16,7 +18,7 @@ public class Solution
                     verticesToProcess.Add((row, col));
                     mat[row][col] = -1;
                 }
-                  
+
             }
         }
 
@@ -41,6 +43,54 @@ public class Solution
         return mat;
     }
 
+#endif
+
+#if false   // Solution that poor performance.
+    public int[][] UpdateMatrix(int[][] mat)
+    {
+        // Initialize verticesToProcess
+        for (int row = 0; row < mat.Length; row++)
+        {
+            for (int col = 0; col < mat[row].Length; col++)
+            {
+                if (mat[row][col] == 1)
+                {
+                    verticesToProcess.Add((row, col));
+                    mat[row][col] = -1;
+                }
+                  
+            }
+        }
+
+        int currentIndex = 0;
+        while (verticesToProcess.Count > 0)
+        {
+            var (r, c) = verticesToProcess[currentIndex];
+            if (doesNeighborExist(r, c, currentDistance - 1, mat))
+            {
+                mat[r][c] = currentDistance;
+                verticesToProcess.Remove((r, c));
+
+                if (currentIndex < verticesToProcess.Count)
+                    continue;
+            }
+            else
+            {
+                if (currentIndex < verticesToProcess.Count - 1)
+                {
+                    currentIndex++;
+                    continue;
+                }
+            }
+            if (currentIndex >= verticesToProcess.Count - 1)
+            {
+                currentIndex = 0;
+                currentDistance++;
+            }
+        }
+        return mat;
+    }
+#endif
     bool doesNeighborExist(int r, int c, int neighborValue, int[][] mat)
     {
         // up
@@ -98,7 +148,7 @@ class Program
         DebugPrintMatrix(input);
 
         int[][] output = test.UpdateMatrix(input);
-    
+
         Console.WriteLine();
 
         Console.WriteLine("Output: ");
@@ -127,23 +177,73 @@ class Program
 
         while (true)
         {
-            // Test Case 1, Expected Output: 2   
+
+#if true
+            Console.WriteLine("Test Case 1: ");
+            // Test Case 1 
             int[][] input1 = new int[][] {
                 new int[] { 0, 1, 1  },
                 new int[] { 1, 1, 1  },
                 new int[] { 1, 1, 1  },
             };
 
-            int[][] expected = new int[][] {
+            int[][] expected1 = new int[][] {
                 new int[] { 0, 1, 2  },
                 new int[] { 1, 2, 3  },
                 new int[] { 2, 3, 4  },
             };
 
-            result = TestCase(input1, expected);
+            result = TestCase(input1, expected1);
             if (!result)
             {
                 Console.WriteLine("Test case 1 failed");
+                break;
+            }
+#endif
+
+#if true
+            Console.WriteLine();
+            Console.WriteLine("Test Case 2: ");
+            // Test Case 2
+            int[][] input2 = new int[][] {
+                new int[] { 0, 0, 0  },
+                new int[] { 0, 1, 0  },
+                new int[] { 1, 1, 1  },
+            };
+
+            int[][] expected2 = new int[][] {
+                new int[] { 0, 0, 0  },
+                new int[] { 0, 1, 0  },
+                new int[] { 1, 2, 1  },
+            };
+
+            result = TestCase(input2, expected2);
+            if (!result)
+            {
+                Console.WriteLine("Test case 2 failed");
+                break;
+            }
+#endif
+
+            Console.WriteLine();
+            Console.WriteLine("Test Case 3: ");
+            // Test Case 3
+            int[][] input3 = new int[][] {
+                new int[] { 0, 1, 0  },
+                new int[] { 0, 1, 0  },
+                new int[] { 0, 1, 0  },
+            };
+
+            int[][] expected3 = new int[][] {
+                new int[] { 0, 1, 0  },
+                new int[] { 0, 1, 0  },
+                new int[] { 0, 1, 0  },
+            };
+
+            result = TestCase(input3, expected3);
+            if (!result)
+            {
+                Console.WriteLine("Test case 3 failed");
                 break;
             }
 
@@ -154,3 +254,4 @@ class Program
         Console.ReadLine();
     }
 }
+
--- MyTest/Program.cs
+++ MyTest/Program.cs
@@ -2,21 +2,26 @@
 {
     public static void Main()
     {
-        float a = 1.239f;
+        List<int> list = new List<int>();
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+        list.Add(4);
+        list.Add(5);
+        list.Add(6);
+        list.Add(7);
+        list.Add(8);
+        list.Add(9);
+        list.Add(10);
 
-        Console.WriteLine(a);
-
-
-        float b = 1234567890123f;
-
-        float c = 12345678901999f;
-        Console.WriteLine(b);
-
-        if (b == c)
-            Console.WriteLine("b equals c");
-        else
+        int currentIndex = 0;
+        while (list.Count > 0)
         {
-            Console.WriteLine("b NOT equals c");
+            Console.WriteLine(list[currentIndex]);
+            list.RemoveAt(0);
+
         }
+
+        Console.WriteLine("Exit");  
     }
 }
