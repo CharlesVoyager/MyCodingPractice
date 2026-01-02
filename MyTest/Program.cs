0d5a04e615a4dbaef8405181f967966250b0cc4a
Charles Chang <charles.chang@live.com>
2026-01-02 12:14:29 +0800

    save

--- MyTest/Program.cs
+++ MyTest/Program.cs
@@ -1,22 +1,74 @@
-﻿class Program
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+public class H2O
 {
-    public static void Main()
+
+    private readonly Barrier _barrier = new Barrier(2);
+    private readonly SemaphoreSlim _semaphore2HDone = new SemaphoreSlim(0);
+
+    static int HydrogenCalledCount = 0;
+
+    public H2O()
     {
-        float a = 1.239f;
 
-        Console.WriteLine(a);
+    }
+
+    public void Hydrogen(Action releaseHydrogen)
+    {
+
+        HydrogenCalledCount++;
+
+        while (HydrogenCalledCount > (_barrier.CurrentPhaseNumber + 1) * 2)
+            Thread.Sleep(10);
+
+        _barrier.SignalAndWait();
+
+        releaseHydrogen();
+
+        Console.WriteLine("HydrogenCalledCount: " + HydrogenCalledCount);
+
+        if (HydrogenCalledCount % 2 == 0)
+            _semaphore2HDone.Release();
+    }
+
+    public void Oxygen(Action releaseOxygen)
+    {
+
+        _semaphore2HDone.Wait();
+
+        // releaseOxygen() outputs "O". Do not change or remove this line.
+        releaseOxygen();
+    }
+}
 
 
-        float b = 1234567890123f;
+class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var service = new H2O();
 
-        float c = 12345678901999f;
-        Console.WriteLine(b);
+        Task[] tasks = new Task[3];
 
-        if (b == c)
-            Console.WriteLine("b equals c");
-        else
+        tasks[0] = Task.Run(() =>
         {
-            Console.WriteLine("b NOT equals c");
-        }
+            service.Hydrogen(() => Console.Write("H"));
+        });
+
+        tasks[1] = Task.Run(() =>
+        {
+            service.Oxygen(() => Console.Write("O"));
+        });
+
+        tasks[2] = Task.Run(() =>
+        {
+            service.Hydrogen(() => Console.Write("H"));
+        });
+
+        // Wait for both threads to finish their work
+        await Task.WhenAll(tasks);
+
+        Console.WriteLine("\n\nExecution finished successfully.");
     }
 }
---
+++ WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    public class FooBar
+    {
+        private readonly SemaphoreSlim _semaphoreFoo = new SemaphoreSlim(0);
+        private readonly SemaphoreSlim _semaphoreBar = new SemaphoreSlim(1);
+        private int n;
+
+        public FooBar(int n)
+        {
+            this.n = n;
+        }
+
+        public void Foo(Action printFoo)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                _semaphoreFoo.Wait();
+
+                printFoo();
+
+                //_semaphoreFoo.Release();
+            }
+        }
+
+        public void Bar(Action printBar)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                _semaphoreFoo.Wait();
+
+                printBar();
+
+                //_semaphoreBar.Release();
+            }
+        }
+
+        public void ReleaseFoo()
+        {
+            int count = _semaphoreFoo.Release();
+            System.Diagnostics.Trace.WriteLine("ReleaseFoo() ==> Count: " + count);
+        }
+
+        public void ReleaseBar()
+        {
+            int count = _semaphoreBar.Release();
+            System.Diagnostics.Trace.WriteLine("ReleaseBar() ==> Count: " + count);
+        }
+    }
+
+    public partial class MainWindow : Window
+    {
+        FooBar service;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+          //  Console.WriteLine("Starting FooBar Server...\n");
+           // System.Diagnostics.Trace.WriteLine("Starting FooBar Server...\n");
+             Main();
+
+        }
+
+        async Task Main()
+        {
+            System.Diagnostics.Trace.WriteLine("Starting FooBar Server...\n");
+            int n = 10; // Number of iterations
+
+            service = new FooBar(n);
+
+            // Create two concurrent tasks (threads)
+            Task threadFoo = Task.Run(() =>
+            {
+                service.Foo(() => PrintLog("foo"));
+            });
+
+            Task threadBar = Task.Run(() =>
+            {
+                service.Bar(() => PrintLog("bar"));
+            });
+
+            // Wait for both threads to finish their work
+            await Task.WhenAll(threadFoo, threadBar);
+
+            System.Diagnostics.Trace.WriteLine("\n\nExecution finished successfully.");
+        }
+
+        void PrintLog(string message)
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                LogTextBox.AppendText(message);
+                LogTextBox.ScrollToEnd();
+            });
+        }
+
+        private void FooRelease_Click(object sender, RoutedEventArgs e)
+        {
+            service.ReleaseFoo();
+        }
+
+        private void BarRelease_Click(object sender, RoutedEventArgs e)
+        {
+            service.ReleaseBar();
+        }
+    }
+}
