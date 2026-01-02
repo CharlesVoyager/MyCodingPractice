2fac790cef444dc91efe8c52df164f6fdfea1f70
Charles Chang <charles.chang@live.com>
2026-01-02 20:00:37 +0800

    submission is  accepted.

--- BuildingH2O/Program.cs
+++ BuildingH2O/Program.cs
@@ -1,31 +1,32 @@
 ﻿// 1117. Building H2O 
 
-using System.Threading;
-using System.Threading.Tasks;
-
 public class H2O
 {
+
     private readonly Barrier _barrier = new Barrier(3);
     private readonly SemaphoreSlim collectH = new SemaphoreSlim(2);
     private readonly SemaphoreSlim collectO = new SemaphoreSlim(1);
 
     private readonly SemaphoreSlim printHDone = new SemaphoreSlim(0);
 
+    private readonly object _lockObject = new object();
+
     public H2O()
     {
-
     }
 
     public void Hydrogen(Action releaseHydrogen)
     {
-
         collectH.Wait();
         _barrier.SignalAndWait();
 
-        releaseHydrogen();
+        lock (_lockObject)
+        {
+            // releaseHydrogen() outputs "H". Do not change or remove this line.
+            releaseHydrogen();
+        }
 
         printHDone.Release();
-
         collectH.Release();
     }
 
@@ -44,40 +45,48 @@ public class H2O
         collectO.Release();
     }
 }
-
 class Program
 {
     public static async Task Main(string[] args)
     {
         var service = new H2O();
 
-        string input = "OOHHHH";
+        //string input = "OOHHHH";
+        string input = "HOHOHH";
+        //string input = "OOHHHHOOHHHHOOHHHHOOHHHH";
+
+
+        Console.WriteLine("Output:");
 
         Task[] tasks = new Task[input.Length];
 
         for (int i = 0; i < input.Length; i++)
         {
-            char atom = input[i];
-            if (atom == 'O')
+            if (input[i] == 'O')
             {
                 tasks[i] = Task.Run(() =>
                 {
-                    service.Oxygen(() => System.Diagnostics.Trace.WriteLine("O"));
-                    Console.Write("O");
+                    service.Oxygen(() => Console.Write("O"));
+                    //System.Diagnostics.Trace.WriteLine("O");
                 });
             }
-            else if (atom == 'H')
+            else if (input[i] == 'H')
             {
                 tasks[i] = Task.Run(() =>
                 {
-                    service.Hydrogen(() => System.Diagnostics.Trace.WriteLine("H"));
-                    Console.Write("H");
+                    service.Hydrogen(() => Console.Write("H"));
+                    //System.Diagnostics.Trace.WriteLine("H");
                 });
             }
         }
 
         // Wait for both threads to finish their work
         await Task.WhenAll(tasks);
+
+        Console.WriteLine("\n\nExpected:");
+
+        for(int i = 0; i < input.Length / 3; i++)
+            Console.Write("HHO");
 
         Console.WriteLine("\n\nExecution finished successfully.");
     }
